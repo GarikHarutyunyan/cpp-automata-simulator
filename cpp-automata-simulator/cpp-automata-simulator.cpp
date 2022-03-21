5b12649dd5d6ce93b10d5ff3373068a8d6188133
Garik <garik.harutyunyan@zevit.net>
2022-03-22 03:27:45 +0400

    add 1-2 demo values

--- cpp-automata-simulator/cpp-automata-simulator.cpp
+++ cpp-automata-simulator/cpp-automata-simulator.cpp
@@ -12,16 +12,27 @@ int main() {
         // Check if the count of "a"s is even.
         Automata aIsEven(aIsEvenConfig);
         input = "ababbabbababababbababababbaaa"; // String in whcih the "a" charachter is used 14 times.
-        output = aIsEven.run(input);
         cout << "Output's last charachter show if the count of 'a' charachters is even. In Input 'a' charachter is used 14 times."<<endl;
+        output = aIsEven.run(input);
         cout << input << "  :  " << output << endl<<endl;
+
+        input = "ababbabbababaababbaaa"; // String in whcih the "a" charachter is used 11 times.
+        output = aIsEven.run(input);
+        cout << "In Input 'a' charachter is used 11 times." << endl;
+        cout << input << "  :  " << output << endl << endl;
 
 
         // Check if string is a valid email.
         Automata isValidEmail(isValidEmailConfig);
         input = "bacbac@abc.cab"; // Valid Email
-        output = isValidEmail.run(input);
         cout << "The Email is valid if it looks like [a,b,c]@[a,b,c].[a,b,c]. Where [a,b,c] includes at least one symbol." << endl;
+        output = isValidEmail.run(input);
+        cout << input << "  :  " << output << endl;
+        input = "@acccabc.cab"; // Valid Email
+        output = isValidEmail.run(input);
+        cout << input << "  :  " << output << endl;
+        input = "acc@.cab"; // Valid Email
+        output = isValidEmail.run(input);
         cout << input << "  :  " << output << endl << endl;
 
         cout << "Check your Email(use only [a,b,c,@,.] symbols) : ";
