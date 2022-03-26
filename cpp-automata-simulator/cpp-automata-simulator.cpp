c985815880a724497f99f54903635a5de2500e26
Garik <garik.harutyunyan@zevit.net>
2022-03-27 03:14:47 +0400

    get rid of using namespace std

--- cpp-automata-simulator/cpp-automata-simulator.cpp
+++ cpp-automata-simulator/cpp-automata-simulator.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include "Automata.h"
 
-using namespace std;
-
 int main() {
     string input, output;
     string aIsEvenConfig = "AIsEvenConfig.txt";
@@ -12,37 +10,37 @@ int main() {
         // Check if the count of "a"s is even.
         Automata aIsEven(aIsEvenConfig);
         input = "ababbabbababababbababababbaaa"; // String in whcih the "a" charachter is used 14 times.
-        cout << "Output's last charachter show if the count of 'a' charachters is even. In Input 'a' charachter is used 14 times."<<endl;
+        std::cout << "Output's last charachter show if the count of 'a' charachters is even. In Input 'a' charachter is used 14 times."<<endl;
         output = aIsEven.run(input);
-        cout << input << "  :  " << output << endl<<endl;
+        std::cout << input << "  :  " << output << endl<<endl;
 
         input = "ababbabbababaababbaaa"; // String in whcih the "a" charachter is used 11 times.
         output = aIsEven.run(input);
-        cout << "In Input 'a' charachter is used 11 times." << endl;
-        cout << input << "  :  " << output << endl << endl;
+        std::cout << "In Input 'a' charachter is used 11 times." << endl;
+        std::cout << input << "  :  " << output << endl << endl;
 
 
         // Check if string is a valid email.
         Automata isValidEmail(isValidEmailConfig);
         input = "bacbac@abc.cab"; // Valid Email
-        cout << "The Email is valid if it looks like [a,b,c]@[a,b,c].[a,b,c]. Where [a,b,c] includes at least one symbol." << endl;
+        std::cout << "The Email is valid if it looks like [a,b,c]@[a,b,c].[a,b,c]. Where [a,b,c] includes at least one symbol." << endl;
         output = isValidEmail.run(input);
-        cout << input << "  :  " << output << endl;
+        std::cout << input << "  :  " << output << endl;
         input = "@acccabc.cab"; // Valid Email
         output = isValidEmail.run(input);
-        cout << input << "  :  " << output << endl;
+        std::cout << input << "  :  " << output << endl;
         input = "acc@.cab"; // Valid Email
         output = isValidEmail.run(input);
-        cout << input << "  :  " << output << endl << endl;
+        std::cout << input << "  :  " << output << endl << endl;
 
-        cout << "Check your Email(use only [a,b,c,@,.] symbols) : ";
+        std::cout << "Check your Email(use only [a,b,c,@,.] symbols) : ";
         cin >> input;
-        cout << endl;
+        std::cout << endl;
         output = isValidEmail.run(input);
-        cout << input << "  :  " << output << endl << endl;
+        std::cout << input << "  :  " << output << endl << endl;
     }
     catch (invalid_argument & err) {
-        cerr << err.what() << endl;
+        std::cerr << err.what() << endl;
     }
 
     return 0;
